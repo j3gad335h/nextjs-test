3db247d0a34013c270a2d02db50e6793376c5695
Jegadeesh <101029948+j3gad335h@users.noreply.github.com>
2023-08-26 10:13:48 +0530

    fonts in public

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,24 @@
-import "@fontsource/tajawal";
 import { CssBaseline } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { ThemeProvider } from "@mui/system";
 import { appWithTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import React, { Suspense, useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import ReactGA from "react-ga4";
 import ScrollToTop from "react-scroll-to-top";
-import "../styles/fonts.css";
+
 const Footer = dynamic(() => import("../components/Footer/Footer"));
 const FooterCopyRights = dynamic(() => import("../components/Footer/FooterCopyRights"));
-import NewHeader from "../components/Header/NewHeader"; // Normal import
+const NewHeader = dynamic(() => import("../components/Header/NewHeader"));
 
 function App({ Component, pageProps }) {
   const { locale } = useRouter();
-  //UA-249956445-1//
-  //UA-250103509-1//
-  // G-JBD2HNXM4S//
-  // G-ELZDSSFFXD
-  //FMD2181M8B
-  const TRACKING_ID = "G-JBD2HNXM4S";
-  // const TRACKING_ID = "UA-250103509-1";
-  ReactGA.initialize(TRACKING_ID);
-  useEffect(() => {
-    document.documentElement.dir = locale === "ar" ? "rtl" : "ltr";
-  }, [locale]);
-
   const theme = createTheme({
     typography: {
-      fontFamily: locale === "ar" ? "Tajawal" : "Ample",
+      fontFamily: locale === "ar" ? "Tajwal-local" : "Ample",
       letterSpacing: "0.00938em !important",
       lineHeight: " 1.5 !important",
     },
@@ -51,6 +39,10 @@ function App({ Component, pageProps }) {
     },
   });
 
+  useEffect(() => {
+    document.documentElement.dir = locale === "ar" ? "rtl" : "ltr";
+  }, [locale]);
+
   return (
     <ThemeProvider theme={theme}>
       <Head>
@@ -58,26 +50,39 @@ function App({ Component, pageProps }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preload" href="/font/Ample-Regular6_0.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+        <link rel="preload" href="/font/Tajawal-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="/globalstyle.css" as="style" />
         <meta name="description" content="Crowdfunding in Saudi Arabia | SME Crowd Lending in Saudi Arabia" />
-        {/* Preload the CSS file */}
-        <link rel="preload" href="/fonts.css" as="style" onload="this.onload=null;this.rel='stylesheet'" />
-        <link rel="preload" href="/font/Ample-Regular6_0.otf" as="font" type="font/woff2" crossorigin="anonymous" />
-        {/* Preload the JavaScript file */}
-        <link rel="preload" href="/styles/Header/HeaderStyle.js" as="script" />
-        <link rel="preload" href="/styles/pages/howitworks/howItWorkStyle.js" as="script" />
-        <noscript>
-          <link rel="stylesheet" href="/fonts.css" />
-        </noscript>
       </Head>
-      <div>
-        <CssBaseline />
-        <NewHeader />
-        <Component {...pageProps} />
-        <Footer />
-        <FooterCopyRights />
-        <ScrollToTop smooth color="#37A753" />
+      <CssBaseline />
+      <div className="page-content">
+        {/* Content that doesn't affect layout */}
+        <div className="header-container">
+          <NewHeader />
+        </div>
+        {/* Main content */}
+        <div className="main-content">
+          {/* Loading state for main content */}
+          <Component {...pageProps} />
+        </div>
+        {/* Footer */}
+        <div className="footer-container">
+          <Footer />
+          <ScrollToTop smooth color="#37A753" />
+          <FooterCopyRights />
+        </div>
       </div>
     </ThemeProvider>
   );
 }
 export default appWithTranslation(App);
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common", "footer"])),
+      // Will be passed to the page component as props
+    },
+  };
+}
--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,25 @@
-import { ServerStyleSheets } from '@mui/styles';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
-import React, { useEffect } from 'react';
+import { ServerStyleSheets } from "@mui/styles";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import React, { useEffect } from "react";
 
 function MyDocument() {
   useEffect(() => {
     // Remove the server-side injected CSS once the client-side takes over
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
 
-
   return (
     <Html>
       <Head>
-        <script dangerouslySetInnerHTML={{
-          __html: `
+        <link rel="preload" href="/font/Ample-Regular6_0.otf" as="font" type="font/otf" crossOrigin="anonymous" />
+        <link rel="preload" href="/font/Tajawal-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="/globalstyle.css" as="style" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
       <script async src="https://www.googletagmanager.com/gtag/js?id=AW-661792686"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
@@ -29,9 +32,11 @@ function MyDocument() {
         gtag("config", "AW-661792686");
       </script>
     `,
-        }} />
-        <script dangerouslySetInnerHTML={{
-          __html: `
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155260236-1"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
@@ -44,7 +49,8 @@ function MyDocument() {
         gtag("config", "UA-155260236-1");
       </script>
     `,
-        }} />
+          }}
+        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -85,10 +91,7 @@ MyDocument.getInitialProps = async (ctx) => {
 
   return {
     ...initialProps,
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
-    ],
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
   };
 };
 
